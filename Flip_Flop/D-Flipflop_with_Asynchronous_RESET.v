94e13c672f77d0dc9342bc222ed8cb9df0a8bae6
Kaushik Baidya <kaushikbaidya02@gmail.com>
2023-08-27 17:35:09 +0530

    D-Flipflop_with_Asynchronous_RESET.v

--- Flip_Flop/D-Flipflop_with_Asynchronous_RESET.v
+++ Flip_Flop/D-Flipflop_with_Asynchronous_RESET.v
@@ -1,4 +1,4 @@
-/*** D-flipflop behavioral positive edge triggered 
+/*** D-flipflop behavioural positive edge triggered 
      with Asynchronous Active High Reset. ***/
 module D_FF_bh (Q, D, Clk, Rst);
     input D, Clk, Rst;
@@ -10,5 +10,20 @@ module D_FF_bh (Q, D, Clk, Rst);
                 Q <= 1'b0;
             else
                 D <= Q;
+        end
+endmodule
+
+/*** D-flipflop behavioural negative edge triggered
+     with Asynchronous Active Low Reset. ***/
+module d_ff_bh (q, d, clk, rst);
+    input d, clk, rst;
+    output reg q;
+
+    always@(negedge clk, negedge rst)
+        begin
+            if(!rst)
+                q <= 1'b0;
+            else
+                q <= d;
         end
 endmodule
